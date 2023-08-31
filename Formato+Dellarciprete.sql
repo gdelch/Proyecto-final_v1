5ca12f2c81f1979b59203881b2350e129b6222a6
gdelch <136943468+gdelch@users.noreply.github.com>
2023-08-31 16:53:21 -0300

    3.- Script de creación de funciones

--- Formato+Dellarciprete.sql
+++ Formato+Dellarciprete.sql
@@ -4,19 +4,23 @@ USE PROYECTO_FINAL_V1;
 /*Esta función devuelve el monto total de ventas de un vendedor, ingresando su id_vendedor*/
 
 DELIMITER //
-CREATE FUNCTION total_ventas (p_id_vendedores INT) 
-RETURNS FLOAT
-NO SQL
+CREATE function
+  fn_total_ventas (p_id_vendedores INT)
+  RETURNOS FLOAT
+  NO SQL
 BEGIN
-DECLARE resultado FLOAT;
-SET resultado = (SELECT SUM(f.cant_venta*p.precio_pub) total FROM facturas f
-JOIN publicaciones p
-ON f.publicacion_id=p.id_publicaciones
-JOIN productos pro
-ON p.producto_id=pro.id_productos
-JOIN vendedores v
-ON pro.vendedor_id=v.id_vendedores
-WHERE v.id_vendedores = p_id_vendedores);
+  DECLARE resultado FLOAT;
+SET resultado =
+  (
+         SELECT sum(f.cant_venta*p.precio_pub) total
+         FROM   facturas f
+         JOIN   publicaciones p
+         ON     f.publicacion_id=p.id_publicaciones
+         JOIN   productos pro
+         ON     p.producto_id=pro.id_productos
+         JOIN   vendedores v
+         ON     pro.vendedor_id=v.id_vendedores
+         WHERE  v.id_vendedores = p_id_vendedores);
 RETURN resultado;
 END
 //
@@ -24,15 +28,19 @@ END
 /*Esta función devuelve el promedio del precio de las publicaciones, ingresando su categoria (vip,exclusive, premium, classic, economic,standard, )*/
 
 DELIMITER //
-CREATE FUNCTION promedio_categoria (p_categoria_exposicion VARCHAR(20)) 
-RETURNS FLOAT
-NO SQL
+CREATE function
+  fn_promedio_categoria (p_categoria_exposicion VARCHAR(20))
+  RETURNS FLOAT
+  NO SQL
 BEGIN
-DECLARE resultado FLOAT;
-SET resultado = (SELECT AVG(p.precio_pub) total FROM publicaciones p
-JOIN exposiciones e
-ON p.exposicion_id=e.id_exposiciones
-WHERE e.categoria_exp = p_categoria_exposicion);
+  DECLARE resultado FLOAT;
+SET resultado =
+  (
+         SELECT avg(p.precio_pub) total
+         FROM   publicaciones p
+         JOIN   exposiciones e
+         ON     p.exposicion_id=e.id_exposiciones
+         WHERE  e.categoria_exp = p_categoria_exposicion);
 RETURN resultado;
 END
 //
