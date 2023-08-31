f7e25e3df36eb14687b1c0b6d6537e0680a2d844
gdelch <136943468+gdelch@users.noreply.github.com>
2023-08-31 17:45:35 -0300

    5.- Creación de triggers

--- Triggers+Dellarciprete.sql
+++ Triggers+Dellarciprete.sql
@@ -21,16 +21,42 @@ CREATE TABLE audit_log_vendedores(
 
 /*Trigger que permite identificar usuario, fecha y hora de la creación de un nuevo vendedor en la BD, junto con su información*/
   
-  DELIMITER //
+DELIMITER //
 CREATE TRIGGER tr_after_insert_vendedor
-AFTER INSERT ON vendedores
-FOR EACH ROW
-BEGIN
-  INSERT INTO audit_log_vendedores(id_vendedores, razonsocial_vendedor, cuit_vendedor, rubro_id, direccion_vendedor,
-  cp_vendedor, telefono_vendedor, email_vendedor, reputacion, accion, fecha_modificacion, hora_modificacion, usuario)
-  VALUES (new.id_vendedores, new.razonsocial_vendedor,
-  new.cuit_vendedor, new.rubro_id, new.direccion_vendedor, new.cp_vendedor, new.telefono_vendedor, new.email_vendedor,
-  new.reputacion,'creación de registro', CURDATE(), CURTIME(),current_user());
+  AFTER INSERT ON vendedores 
+  FOR EACH ROW BEGIN
+  INSERT INTO audit_log_vendedores
+              (
+                          id_vendedores,
+                          razonsocial_vendedor,
+                          cuit_vendedor,
+                          rubro_id,
+                          direccion_vendedor,
+                          cp_vendedor,
+                          telefono_vendedor,
+                          email_vendedor,
+                          reputacion,
+                          accion,
+                          fecha_modificacion,
+                          hora_modificacion,
+                          usuario
+              )
+              VALUES
+              (
+                          new.id_vendedores,
+                          new.razonsocial_vendedor,
+                          new.cuit_vendedor,
+                          new.rubro_id,
+                          new.direccion_vendedor,
+                          new.cp_vendedor,
+                          new.telefono_vendedor,
+                          new.email_vendedor,
+                          new.reputacion,
+                          'creación de registro',
+                          CURDATE(),
+                          CURTIME(),
+                          CURRENT_USER()
+              );
 END;
 //
 DELIMITER ;
@@ -38,16 +64,43 @@ DELIMITER ;
 
 /*Trigger que permite identificar usuario, fecha y hora de la modificación de un vendedor en la BD, guardando su información original de backup*/
 
-  DELIMITER //
-CREATE TRIGGER tr_before_insert_vendedor
-BEFORE UPDATE ON vendedores
-FOR EACH ROW
-BEGIN
-  INSERT INTO audit_log_vendedores(id_vendedores, razonsocial_vendedor, cuit_vendedor, rubro_id, direccion_vendedor,
-  cp_vendedor, telefono_vendedor, email_vendedor, reputacion, accion, fecha_modificacion, hora_modificacion, usuario)
-  VALUES (old.id_vendedores, old.razonsocial_vendedor,
-  old.cuit_vendedor, old.rubro_id, old.direccion_vendedor, old.cp_vendedor, old.telefono_vendedor, old.email_vendedor,
-  old.reputacion,'actualización de registro', CURDATE(), CURTIME(),current_user());
+   DELIMITER //
+  CREATE TRIGGER tr_before_insert_vendedor
+  BEFORE UPDATE ON vendedores 
+  FOR EACH ROW
+  BEGIN
+  INSERT INTO audit_log_vendedores
+              (
+                          id_vendedores,
+                          razonsocial_vendedor,
+                          cuit_vendedor,
+                          rubro_id,
+                          direccion_vendedor,
+                          cp_vendedor,
+                          telefono_vendedor,
+                          email_vendedor,
+                          reputacion,
+                          accion,
+                          fecha_modificacion,
+                          hora_modificacion,
+                          usuario
+              )
+              VALUES
+              (
+                          old.id_vendedores,
+                          old.razonsocial_vendedor,
+                          old.cuit_vendedor,
+                          old.rubro_id,
+                          old.direccion_vendedor,
+                          old.cp_vendedor,
+                          old.telefono_vendedor,
+                          old.email_vendedor,
+                          old.reputacion,
+                          'actualización de registro',
+                          curdate(),
+                          curtime(),
+                          CURRENT_USER()
+              );
 END;
 //
 DELIMITER ;
@@ -68,27 +121,62 @@ CREATE TABLE audit_log_exposiciones(
   /*Trigger que permite identificar usuario, fecha y hora de la creación de una nueva categoria de exposicion, junto con su información*/
   
   
-  DELIMITER //
+DELIMITER //
 CREATE TRIGGER tr_after_insert_exposiciones
-AFTER INSERT ON exposiciones
-FOR EACH ROW
-BEGIN
-  INSERT INTO audit_log_exposiciones(id_exposiciones, categoria_exp, comision_exp, accion, fecha_modificacion, hora_modificacion, usuario)
-  VALUES (new.id_exposiciones, new. categoria_exp, new.comision_exp,'creación de registro', CURDATE(), CURTIME(),current_user());
+  AFTER INSERT ON exposiciones 
+  FOR EACH ROW 
+  BEGIN
+  INSERT INTO audit_log_exposiciones
+              (
+                          id_exposiciones,
+                          categoria_exp,
+                          comision_exp,
+                          accion,
+                          fecha_modificacion,
+                          hora_modificacion,
+                          usuario
+              )
+              VALUES
+              (
+                          new.id_exposiciones,
+                          new. categoria_exp,
+                          new.comision_exp,
+                          'creación de registro',
+                          CURDATE(),
+                          CURTIME(),
+                          CURRENT_USER()
+              );
 END;
 //
 DELIMITER ;
   
-/*Trigger que permite identificar usuario, fecha y hora de modificación de una categoria de exposicion, junto con su información previa al cambio como backup*/
+/*Trigger que permite identificar usuario, fecha y hora de modificación de una categoria de exposicion, junto con su información previa al cambio como backup*/ 
 
-
-  DELIMITER //
+DELIMITER //
 CREATE TRIGGER tr_before_insert_exposiciones
-BEFORE UPDATE ON exposiciones
-FOR EACH ROW
-BEGIN
-  INSERT INTO audit_log_exposiciones(id_exposiciones, categoria_exp, comision_exp, accion, fecha_modificacion, hora_modificacion, usuario)
-  VALUES (old.id_exposiciones, old. categoria_exp, old.comision_exp,'actualización de registro', CURDATE(), CURTIME(),current_user());
+  BEFORE
+  UPDATE
+  ON exposiciones FOR EACH row begin
+  INSERT INTO audit_log_exposiciones
+              (
+                          id_exposiciones,
+                          categoria_exp,
+                          comision_exp,
+                          accion,
+                          fecha_modificacion,
+                          hora_modificacion,
+                          usuario
+              )
+              VALUES
+              (
+                          old.id_exposiciones,
+                          old. categoria_exp,
+                          old.comision_exp,
+                          'actualización de registro',
+                          curdate(),
+                          curtime(),
+                          CURRENT_USER()
+              );
 END;
 //
 DELIMITER ;
